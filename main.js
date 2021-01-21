c4718e662377ff936a616f3ad220fbef69f662ad
Jpeyton <jpwalk319@gmail.com>
2021-01-21 08:24:22 -0700

    fixed a bug which was not populating the city variable due to a scope error amd changed the submit button to a button instead of a submit input

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const apiKey = '3f2fb54412fdff446633a41487bb3d44';
-const city = 'kansas city';
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=" + city + "&units=imperial&appid=" + apiKey;
- 
-console.log('connected');
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response){
-    console.log(response);
+
+const searchBtn = $('#submit');
+
+
+function getWeather() {
+    let city = $('#cityname').val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+     city + "&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response)
+    })
+}
+
+searchBtn.click(function(e){
+    e.preventDefault();
+    getWeather();   
 })
