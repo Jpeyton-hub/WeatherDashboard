1dbd2025ed8b2ea3b2c1e094f71b5881c1b521df
Jpeyton <jpwalk319@gmail.com>
2021-01-21 10:16:47 -0700

    added datejs cdn and added date variable to main.js as well as date parsing to the getweather function, need to adjust for timezone still

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const apiKey = '3f2fb54412fdff446633a41487bb3d44';
 let weatherDiv = $('#weather');
-
+let now = new Date()
 const searchBtn = $('#submit');
 
 
@@ -12,13 +12,15 @@ function getWeather() {
         url: queryURL,
         method: 'GET'
     }).then(function(response){
-        console.log(response)        
+        console.log(response)
+        let timezone = response.timezone;
+        let localTime = new Date(now.getTime() + timezone);       
         let temp = 'Temperature: ' + response.main.temp + '°F';
         let humid = 'Humidity: ' + response.main.humidity + '%';
         let wind = 'Windspeed: ' + response.wind.speed + 'mph';
         let lat = response.coord.lat;
         let lon = response.coord.lon;        
-        let infoArr = [temp, humid, wind];
+        let infoArr = [localTime, temp, humid, wind];
         for (let i = 0; i < infoArr.length; i++) {
             let info = $('<p>').text(infoArr[i]);
             weatherDiv.append(info);
