356b46b807f8a8003cb12ffe64847a7d9f175af4
Jpeyton <jpwalk319@gmail.com>
2021-01-21 11:52:54 -0700

    removed datejs from indexhtml as it wasn't necessary and finished function to display local date and time

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const apiKey = '3f2fb54412fdff446633a41487bb3d44';
 let weatherDiv = $('#weather');
-let now = new Date()
 const searchBtn = $('#submit');
 
 
@@ -11,20 +10,37 @@ function getWeather() {
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(function(response){
-        console.log(response)
-        let timezone = response.timezone;
-        let localTime = new Date(now.getTime() + timezone);       
-        let temp = 'Temperature: ' + response.main.temp + '°F';
-        let humid = 'Humidity: ' + response.main.humidity + '%';
-        let wind = 'Windspeed: ' + response.wind.speed + 'mph';
+    }).then(function(response){    
+        //first api call to grab lat and long for the more detailed second call
         let lat = response.coord.lat;
         let lon = response.coord.lon;        
-        let infoArr = [localTime, temp, humid, wind];
-        for (let i = 0; i < infoArr.length; i++) {
-            let info = $('<p>').text(infoArr[i]);
-            weatherDiv.append(info);
-        };
+        //second call below contains more detailed current conditions and the forecast, but must be called using latitude and longitude.
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&appid=' + apiKey,
+            method : 'GET'
+        }).then(function(response){
+            console.log(response);
+            //datadump for weather div which should contain current conditions
+            
+            // let now = new Date();
+            // let currentTime = now.getTime();
+            // let currentUTC = currentTime + (now.getTimezoneOffset() * 60000);
+            // let destTime = currentUTC + (response.timezone_offset * 1000);            
+            let convertedDestTime = new Date(response.current.dt * 1000);
+            let localTime = convertedDestTime.toLocaleString('en-US');
+            let temp = 'Temperature: ' + response.current.temp + '°F';
+            let humid = 'Humidity: ' + response.current.humidity + '%';
+            let wind = 'Windspeed: ' + response.current.wind_speed + 'mph';
+            let uv = 'UV Index: ' + response.current.uvi;
+            let infoArr = [localTime, temp, humid, wind, uv];
+            //for loop to append current conditions to weather div
+            for (let i = 0; i < infoArr.length; i++) {
+                let info = $('<p>').text(infoArr[i]);
+                weatherDiv.append(info);
+            };
+            //datadump for forecast
+
+        })
     })
 }
 
