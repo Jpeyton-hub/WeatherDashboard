0c90283011c9d28fa4386592e0ebe7d40be0c680
Jpeyton <jpwalk319@gmail.com>
2021-01-21 15:28:32 -0700

    added styling for h5 inside the current div and fixed bug causing the time to always be my current time instead of the time at target location

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ function getWeather() {
         method: 'GET'
     }).then(function(response){    
         //first api call to grab lat and long for the more detailed second call
-        
+        weatherDiv.empty();
         let location = response.name;
+        let nameInfo = $('<h5>').text(location);
+        weatherDiv.append(nameInfo);
         let lat = response.coord.lat;
         let lon = response.coord.lon;        
         //second call below contains more detailed current conditions and the forecast, but must be called using latitude and longitude.
@@ -21,23 +23,28 @@ function getWeather() {
             url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&appid=' + apiKey,
             method : 'GET'
         }).then(function(response){
-            weatherDiv.empty();
+            
             console.log(response);
-            //datadump for weather div which should contain current conditions             
-            let convertedDestTime = new Date(response.current.dt * 1000);
+            //datadump for weather div which should contain current conditions
+            let now = new Date();
+            let currentMilli = now.getTime();
+            let localOffset = now.getTimezoneOffset() * 60000;
+            let utc = currentMilli + localOffset;
+            let destTime = utc + (1000 * response.timezone_offset);
+            let convertedDestTime = new Date(destTime);
             let localTime = convertedDestTime.toLocaleString('en-US');
             let temp = 'Temperature: ' + response.current.temp + '°F';
             let humid = 'Humidity: ' + response.current.humidity + '%';
             let wind = 'Windspeed: ' + response.current.wind_speed + 'mph';
             let uv = 'UV Index: ' + response.current.uvi;
-            let infoArr = [location, localTime, temp, humid, wind, uv];
+            let infoArr = [localTime, temp, humid, wind, uv];
             //for loop to append current conditions to weather div
             for (let i = 0; i < infoArr.length; i++) {
                 let info = $('<p>').text(infoArr[i]);
                 weatherDiv.append(info);
             };
             //datadump for forecast
-
+            
         })
     })
 }
